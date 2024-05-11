5d0c97d7dd6c86abdddff86b870e73ad85ecb0a3
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-05-12 01:11:48 +0600

    sort functionality done on available page

--- src/pages/AvailableFoods/AvailableFoods.jsx
+++ src/pages/AvailableFoods/AvailableFoods.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 import { BsFillGrid3X3GapFill, BsGridFill } from "react-icons/bs";
 import FoodCard from "../../components/FoodCard";
 import Loader from "../../components/Loader";
@@ -37,17 +38,30 @@ const AvailableFoods = () => {
     }
     setAllItems(res.data);
   };
+
+  const handleSort = async (url) => {
+    const res = await axiosSecure.get(`/sortFoods/${url}`);
+    setAllItems(res.data);
+  };
+
+  const handleAllFood = async () => {
+    const res = await axiosSecure.get("/availableFoods");
+    setAllItems(res.data);
+  };
   //   const { foods, isPending } = useFoods("availableFoods");
 
   return (
     <div>
+      <Helmet>
+        <title>JR-Botanica | AvailableFoods</title>
+      </Helmet>
       <h2 className="text-center font-bold text-[#84d814] text-4xl mt-5">
         ALL FOODS HERE
       </h2>
-      <div className="flex gap-2 justify-between items-center">
+      <div className="flex flex-col items-center gap-9 md:flex-row md:gap-2 md:justify-around md:items-center my-10">
         <div>
           <form onSubmit={handleSearchSubmit} className="flex">
-            <label className="flex items-center gap-2 w-[300px] focus:outline-none">
+            <label className="flex items-center gap-2 w-[250px] focus:outline-none">
               <input
                 type="text"
                 className="focus:outline-none input input-bordered w-full focus:border-[#84d814]"
@@ -63,7 +77,46 @@ const AvailableFoods = () => {
             />
           </form>
         </div>
-        <div className="flex gap-4">
+
+        <div className="flex gap-4 items-center">
+          <div className="dropdown dropdown-hover">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn m-1 bg-[#84d814] hover:bg-[#84d814] text-white"
+            >
+              Sort by Date
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 pl-0 bg-base-100 rounded-box w-52 space-y-2"
+            >
+              <li
+                className="btn hover:bg-[#84d814] hover:text-[#FFFF]"
+                onClick={() => handleSort("ascending")}
+              >
+                Ascending
+              </li>
+              <li
+                className="btn hover:bg-[#84d814] hover:text-[#FFFF]"
+                onClick={() => handleSort("descending")}
+              >
+                Descending
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <button
+              onClick={() => handleAllFood()}
+              className="btn bg-[#84d814] hover:bg-[#84d814] text-white"
+            >
+              All Foods
+            </button>
+          </div>
+        </div>
+
+        <div className="md:flex gap-4  hidden">
           <BsFillGrid3X3GapFill
             className={`cursor-pointer ${layoutIndex === 0 && `border`}`}
             onClick={() => setLayoutIndex(0)}
