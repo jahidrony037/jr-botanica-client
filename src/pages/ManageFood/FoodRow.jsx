dd89b63da58b228770a9e1eb72ea9a9bccf3a0e4
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-05-15 11:51:53 +0600

    tooltip added

--- src/pages/ManageFood/FoodRow.jsx
+++ src/pages/ManageFood/FoodRow.jsx
@@ -1,5 +1,8 @@
 import PropType from "prop-types";
+import { PiNotePencilLight } from "react-icons/pi";
+import { RiDeleteBin6Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { Tooltip } from "react-tooltip";
 import useFormatDate from "../../hooks/useFormatDate";
 const FoodRow = ({ food, i, handleDelete }) => {
   const { donator_name, food_quantity, food_name, expired_date, _id } =
@@ -12,18 +15,24 @@ const FoodRow = ({ food, i, handleDelete }) => {
       <td>{food_quantity}</td>
       <td>{donator_name}</td>
       <td>{useFormatDate(expired_date)}</td>
-      <td className="flex gap-5">
+      <td className="flex gap-5 items-center">
         <Link
           to={`/updateFood/${_id}`}
-          className="btn  rounded-lg hover:bg-[#84d814] bg-[#84d814] text-white"
+          // className="btn  rounded-lg hover:bg-[#84d814] bg-[#84d814] text-white"
+          data-tooltip-id="update"
+          data-tooltip-content="UPDATE"
         >
-          update
+          <PiNotePencilLight size={30} />
+          <Tooltip id="update" style={{ backgroundColor: "#84d814" }} />
         </Link>
         <button
           onClick={() => handleDelete(_id)}
-          className="btn btn-error  rounded-lg text-white"
+          // className="btn btn-error  rounded-lg text-white"
+          data-tooltip-id="delete"
+          data-tooltip-content="DELETE"
         >
-          delete
+          <RiDeleteBin6Line size={30} />
+          <Tooltip id="delete" style={{ backgroundColor: "#84d814" }} />
         </button>
       </td>
     </tr>
